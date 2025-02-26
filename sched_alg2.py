70d2d40651b99cbc9d8b7e5775f8912e76da18dc
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2025-02-26 14:21:45 -0600

    Add SJF

--- sched_alg2.py
+++ sched_alg2.py
@@ -1,10 +1,39 @@
 #Abbie Vance and Evan Vance
 #SJF
+from lib.Queue import Queue
+from lib.Processes import SJFProcess
+import random
 
 #CLASSES GO HERE
-
+class SFJScheduler:
+    def __init__(self, queue):
+        self.queue = queue
+    def schedule(self):
+        current = self.queue.head;  
+        index = None;  
+        if(self.queue.is_empty()):
+            print('Queue is empty')
+        elif(self.queue.head == None):  
+            return;  
+        else:  
+            while(current != None):  
+                index = current.next;  
+                  
+                while(index != None):  
+                    if(current.item < index.item):  
+                        temp = current.item;  
+                        current.item = index.item;  
+                        index.item = temp;  
+                    index = index.next;  
+                current = current.next; 
 
 if __name__ == '__main__':
 #REMOVE PASS AND DEMONSTRATE THE SCHEDULER IN ACTION HERE
 #CREATE ANY QUEUES, PROCESSES, CALL APPROPRIATE METHODS
-    pass
+    queue = Queue()
+    for i in range(30):
+        queue.put(SJFProcess(pid=random.randint(1,50), burst_time=random.randint(1,50)))
+    scedule = SFJScheduler(queue)
+    scedule.schedule()
+    for i in range(queue.count):
+        print(queue.get())
